1cb483b0997db32b2043ddcbba4f49aa0a1227ae
Olgasinkevich <sinkevichweb@gmail.com>
2021-09-05 22:13:18 +0300

    feat: implement guess game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.result = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.result = Math.round((this.min + this.max) / 2)
+        return this.result;
     }
 
     lower() {
-
+        this.setRange(this.min, this.result);
     }
 
     greater() {
-
+        this.setRange(this.result, this.max);
     }
 }
 
